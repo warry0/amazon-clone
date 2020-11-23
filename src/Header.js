c2e49ab580e5fea3b36ba2a5179ad238973595f4
warry0 <fathalla.amine@free.fr>
2020-11-23 14:45:28 +0100

    Header

---
+++ src/Header.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon"></SearchIcon>
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className ="header__optionLineOne">
+                        Bonjour, identifier-vous
+                    </span>
+                    <span className ="header__optionLineTow">
+                        Connexion
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className ="header__optionLineOne">
+                        Retours
+                    </span>
+                    <span className ="header__optionLineTow">
+                        et Commandes
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className ="header__optionLineOne">
+                        Tester
+                    </span>
+                    <span className ="header__optionLineTow">
+                        Prime
+                    </span>
+                </div> 
+                <div className="header__optionBasket">
+                    <ShoppingCartIcon></ShoppingCartIcon>
+                    <span className="header__optionLineTwo header__basketCount">
+                        0
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCSMnysNiZV7x0EMatdI6_0iYwoDZT8wEk",
+    authDomain: "challenge-d8452.firebaseapp.com",
+    databaseURL: "https://challenge-d8452.firebaseio.com",
+    projectId: "challenge-d8452",
+    storageBucket: "challenge-d8452.appspot.com",
+    messagingSenderId: "429994738459",
+    appId: "1:429994738459:web:93dfbd76f5f8a1825349be",
+    measurementId: "G-0Z26KJTENB"
+  };
